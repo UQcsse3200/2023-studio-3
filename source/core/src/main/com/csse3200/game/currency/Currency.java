5ff59ed3b18a50bc008c9b9e29d5d5515c186183
ryanmcneilly <ryanmcn@live.com.au>
2023-08-16 16:56:49 +1000

    Added methods to the currency class. The currency class can now modify the amount of currency it has, and check if certain items can be bought.

--- source/core/src/main/com/csse3200/game/currency/Currency.java
+++ source/core/src/main/com/csse3200/game/currency/Currency.java
@@ -2,4 +2,47 @@ package com.csse3200.game.currency;
 
 public abstract class Currency {
 
+    // The logo of the currency
+    private final String logoFilePath;
+
+    // Stores the amount of currency there is
+    private int amount;
+
+    /**
+     * Constructor for the currency object.
+     * @param logoFilePath
+     */
+    public Currency(String logoFilePath) {
+        this.logoFilePath = logoFilePath;
+    }
+
+    /**
+     * Getter for the logo of the currency.
+     *
+     * @return the logo of the currency in a Texture format.
+     */
+    public String getTexture() {
+        return this.logoFilePath;
+    }
+
+    /**
+     * This method increments the currency amount by a given amount.
+     * Added amount is assumed to be larger than negative the currency's amount.
+     *
+     * @requires addedAmount >= -this.amount
+     * @param addedAmount The amount the currency will be incremented by.
+     */
+    public void modify(int addedAmount) {
+        this.amount += addedAmount;
+    }
+
+    /**
+     * Checks if you can buy some item with a given cost.
+     *
+     * @param cost the cost of the item.
+     * @return boolean representing if you can buy that object or not.
+     */
+    public boolean canBuy(int cost) {
+        return this.amount >= cost;
+    }
 }
