ff7cfc19fe81074a474abc5dddee9c3c4d1c9386
Mohamad <moh.dabboussi@gmail.com>
2023-08-18 20:05:40 +1000

    Added comments in TowerFactory

--- source/core/src/main/com/csse3200/game/entities/factories/TowerFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/TowerFactory.java
@@ -8,13 +8,22 @@ import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 
-
+/**
+ * Factory to create a tower entity.
+ *
+ * Predefined tower properties are loaded from a config stored as a json file and should have
+ * the properties stores in 'baseTowerConfigs'.
+ */
 public class TowerFactory {
    /**
       private static final baseTowerConfigs configs =
             FileLoader.readClass(baseTowerConfigs.class, "configs/tower.json");
     */
 
+    /**
+     * Creates a generic tower entity to be used as a base entity by more specific tower creation methods.
+     * @return entity
+     */
     public static Entity createBaseTower() {
         // we're going to add more components later on
         Entity tower = new Entity()
