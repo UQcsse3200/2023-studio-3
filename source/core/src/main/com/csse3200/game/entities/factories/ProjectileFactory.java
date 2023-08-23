9e316ccc362d4893ac37fad2fd139393479ecce9
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-08-23 10:01:15 +1000

    Add and configure collider and physics components to projectile factory class

--- source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
@@ -2,23 +2,35 @@ package com.csse3200.game.entities.factories;
 
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.rendering.TextureRenderComponent;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.PhysicsUtils;
+import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.HitboxComponent;
+import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.physics.components.PhysicsMovementComponent;
 
 /**
  * Responsible for creating projectiles within the game
  */
 public class ProjectileFactory {
-    public static Entity createProjectile() {
-        Entity projectile = 
-            new Entity().addComponent(new TextureRenderComponent("images/projectile.png"));
-        
-        projectile.getComponent(TextureRenderComponent.class).scaleEntity();
-        return projectile;
-    }
+  public static Entity createProjectile() {
+    Entity projectile = new Entity()
+        .addComponent(new TextureRenderComponent("images/projectile.png"))
+        .addComponent(new PhysicsComponent())
+        .addComponent(new PhysicsMovementComponent())
+        .addComponent(new HitboxComponent().setLayer(PhysicsLayer.PROJECTILE))
+        .addComponent(new ColliderComponent());
 
-    /**
-     * Prevents the creation of a Projectile Factory entity being created
-     */
-    private ProjectileFactory() {
-        throw new IllegalStateException("Instantiating static util class");
-    }
+    projectile.getComponent(TextureRenderComponent.class).scaleEntity();
+
+    PhysicsUtils.setScaledCollider(projectile, 0.9f, 0.4f);
+    return projectile;
+  }
+
+  /**
+   * Prevents the creation of a Projectile Factory entity being created
+   */
+  private ProjectileFactory() {
+    throw new IllegalStateException("Instantiating static util class");
+  }
 }
