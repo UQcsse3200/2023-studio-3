fa94aafdb80ddb4f2f7626ce3aa2460941709b23
Ahmad Abu-Aysha <s4638852@student.uq.edu.au>
2023-08-24 10:31:33 +1000

    created TowerAnimationController class

--- source/core/src/main/com/csse3200/game/components/tower/TowerAnimationController.java
+++ source/core/src/main/com/csse3200/game/components/tower/TowerAnimationController.java
@@ -4,7 +4,7 @@ import com.csse3200.game.components.Component;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 
 /**
- * This class listens to events relevant to a ghost entity's state and plays the animation when one
+ * This class listens to events relevant to a weapon tower state and plays the animation when one
  * of the events is triggered.
  */
 public class TowerAnimationController extends Component {
@@ -14,15 +14,26 @@ public class TowerAnimationController extends Component {
     public void create() {
         super.create();
         animator = this.entity.getComponent(AnimationRenderComponent.class);
-        entity.getEvents().addListener("wanderStart", this::animateWander);
-        entity.getEvents().addListener("chaseStart", this::animateChase);
+        entity.getEvents().addListener("idleStart", this::animateIdle);
+        entity.getEvents().addListener("deployStart", this::animateDeploy);
+        entity.getEvents().addListener("stowStart", this::animateStow);
+        entity.getEvents().addListener("firingStart", this::animateFiring);
+
     }
 
-    void animateWander() {
+    void animateIdle() {
         animator.startAnimation("idle");
     }
 
-    void animateChase() {
-        animator.startAnimation("angry");
+    void animateStow() {
+        animator.startAnimation("stow");
+    }
+
+    void animateDeploy() {
+        animator.startAnimation("deploy");
+    }
+
+    void animateFiring() {
+        animator.startAnimation("firing");
     }
 }
