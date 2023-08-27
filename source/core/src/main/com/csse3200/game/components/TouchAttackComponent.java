0d9897972497481f3ce62918353b95a04454a159
max9753 <106744741+max9753@users.noreply.github.com>
2023-08-27 11:36:49 +1000

    Revert "Feature/team4/base enemy class"

--- source/core/src/main/com/csse3200/game/components/TouchAttackComponent.java
+++ source/core/src/main/com/csse3200/game/components/TouchAttackComponent.java
@@ -63,12 +63,7 @@ public class TouchAttackComponent extends Component {
     Entity target = ((BodyUserData) other.getBody().getUserData()).entity;
     CombatStatsComponent targetStats = target.getComponent(CombatStatsComponent.class);
     if (targetStats != null) {
-        // If entity has abilities, pick one at random and apply it else use baseAttack damage
-        if (combatStats.getWeapon(target) != null) {
-            targetStats.hit(combatStats.getWeapon(target).getDamage());
-        } else {
-            targetStats.hit(combatStats.getBaseAttack());
-        }
+      targetStats.hit(combatStats);
     }
 
     // Apply knockback
