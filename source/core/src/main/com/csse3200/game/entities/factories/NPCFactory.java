162e177b4d55fde12adc824f4942747cde1d2d38
BlairCannon97 <blair.cannon@uq.net.au>
2023-08-27 16:19:18 +1000

    Set the spawn of trees and ghosts to 0 for easier modification. Xeno grunt now being built using the base enemy config

--- source/core/src/main/com/csse3200/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/NPCFactory.java
@@ -11,6 +11,7 @@ import com.csse3200.game.components.TouchAttackComponent;
 import com.csse3200.game.components.tasks.ChaseTask;
 import com.csse3200.game.components.tasks.WanderTask;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.BaseEnemyConfig;
 import com.csse3200.game.entities.configs.BaseEntityConfig;
 import com.csse3200.game.entities.configs.GhostKingConfig;
 import com.csse3200.game.entities.configs.NPCConfigs;
@@ -98,7 +99,7 @@ public class NPCFactory {
    */
   public static Entity createXenoGrunt(Entity target) {
     Entity xenoGrunt = createBaseNPC(target);
-    BaseEntityConfig config = configs.xenoGrunt;
+    BaseEnemyConfig config = configs.xenoGrunt;
 
     AnimationRenderComponent animator =
             new AnimationRenderComponent(
