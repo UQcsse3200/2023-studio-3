1f1f79b26b8adcb18e7e93539a62f7a9d11d5458
Nawal <nawalnazim@gmail.com>
2023-08-28 20:07:47 +1000

    Added tests for Currency and CurrencyService classes

--- source/core/src/test/com/csse3200/game/currency/CurrencyTest.java
+++ source/core/src/test/com/csse3200/game/currency/CurrencyTest.java
@@ -4,7 +4,7 @@ import com.csse3200.game.extensions.GameExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(GameExtension.class)
@@ -16,6 +16,24 @@ class CurrencyTest {
         int value = 100;
         currency.setAmount(value);
         assertEquals(value, currency.getAmount());
+    }
+
+    @Test
+    void shouldModifyAmount() {
+        Currency currency = mock(Currency.class, CALLS_REAL_METHODS);
+        int value = 100;
+        currency.setAmount(value);
+        currency.modify(value);
+        assertEquals(200, currency.getAmount());
+    }
+
+    @Test
+    void testCanBuy() {
+        Currency currency = mock(Currency.class, CALLS_REAL_METHODS);
+        currency.setAmount(100);
+
+        assertFalse(currency.canBuy(200));
+        assertTrue(currency.canBuy(60));
     }
 
 }
