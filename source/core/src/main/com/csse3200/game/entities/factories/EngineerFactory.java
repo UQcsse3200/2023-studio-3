5663ba6a2d07c6483e2a7b89b4f4f0c51dd79dff
Ahmad Abu-Aysha <s4638852@student.uq.edu.au>
2023-09-05 23:03:21 +1000

    created Engineer Factory class

---
+++ source/core/src/main/com/csse3200/game/entities/factories/EngineerFactory.java
@@ -0,0 +1,105 @@
+package com.csse3200.game.entities.factories;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.ai.tasks.AITaskComponent;
+import com.csse3200.game.components.CombatStatsComponent;
+import com.csse3200.game.components.TouchAttackComponent;
+import com.csse3200.game.components.npc.GhostAnimationController;
+import com.csse3200.game.components.npc.XenoAnimationController;
+import com.csse3200.game.components.player.HumanAnimationController;
+import com.csse3200.game.components.tasks.ShootTask;
+import com.csse3200.game.components.tasks.WanderTask;
+import com.csse3200.game.components.tasks.human.HumanWanderTask;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.Melee;
+import com.csse3200.game.entities.PredefinedWeapons;
+import com.csse3200.game.entities.Weapon;
+import com.csse3200.game.entities.configs.*;
+import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.PhysicsUtils;
+import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.HitboxComponent;
+import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.physics.components.PhysicsMovementComponent;
+import com.csse3200.game.rendering.AnimationRenderComponent;
+import com.csse3200.game.rendering.TextureRenderComponent;
+import com.csse3200.game.services.ServiceLocator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Factory to create non-playable character (NPC) entities with predefined components.
+ *
+ * <p>Each NPC entity type should have a creation method that returns a corresponding entity.
+ * Predefined entity properties can be loaded from configs stored as json files which are defined in
+ * "NPCConfigs".
+ *
+ * <p>If needed, this factory can be separated into more specific factories for entities with
+ * similar characteristics.
+ */
+public class EngineerFactory {
+  private static final EngineerConfigs configs =
+      FileLoader.readClass(EngineerConfigs.class, "configs/Engineers.json");
+
+  /**
+   * Creates an Engineer entity.
+   *
+   * @return entity
+   */
+  public static Entity createEngineer() {
+    Entity engineer = createBaseHumanNPC();
+    BaseEntityConfig config = configs.engineer;
+
+    AnimationRenderComponent animator = new AnimationRenderComponent(
+            new TextureAtlas("images/engineers/engineer.atlas"));
+    animator.addAnimation("walk_left", 0.2f, Animation.PlayMode.LOOP);
+    animator.addAnimation("walk_right", 0.2f, Animation.PlayMode.LOOP);
+    animator.addAnimation("idle_right", 0.2f, Animation.PlayMode.LOOP);
+    animator.addAnimation("firing", 0.1f, Animation.PlayMode.NORMAL);
+    animator.addAnimation("hit", 0.1f, Animation.PlayMode.NORMAL);
+    animator.addAnimation("death", 0.1f, Animation.PlayMode.NORMAL);
+
+
+    engineer
+        .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
+        .addComponent(animator)
+        .addComponent(new HumanAnimationController());
+
+    engineer.getComponent(AnimationRenderComponent.class).scaleEntity();
+    engineer.setScale(1.5f, 1.2f);
+    return engineer;
+  }
+
+  /**
+   * Creates a generic human npc to be used as a base entity by more specific NPC creation methods.
+   *
+   * @return entity
+   */
+  public static Entity createBaseHumanNPC() {
+    AITaskComponent aiComponent =
+        new AITaskComponent()
+            .addTask(new HumanWanderTask(new Vector2(0, 10f), 2f));
+
+    Entity human =
+        new Entity()
+            .addComponent(new PhysicsComponent())
+            .addComponent(new PhysicsMovementComponent())
+            .addComponent(new ColliderComponent())
+            .addComponent(new HitboxComponent().setLayer(PhysicsLayer.PLAYER))
+            .addComponent(new TouchAttackComponent(PhysicsLayer.NPC, 1.5f))
+            .addComponent(aiComponent);
+
+    PhysicsUtils.setScaledCollider(human, 0.9f, 0.4f);
+    return human;
+  }
+
+  private EngineerFactory() {
+    throw new IllegalStateException("Instantiating static util class");
+  }
+}
+
+
