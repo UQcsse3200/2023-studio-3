c41f762c60b5ea0aa218688fd03a3eaecfa1c129
aadityayadav17 <yadavaadi17@icloud.com>
2023-09-07 19:30:26 +1000

    Map Boundary Fixed

--- source/core/src/main/com/csse3200/game/utils/math/GridPoint2Utils.java
+++ source/core/src/main/com/csse3200/game/utils/math/GridPoint2Utils.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.GridPoint2;
  * Contains additional utility constants and functions for common GridPoint2 operations.
  */
 public class GridPoint2Utils {
-  public static final GridPoint2 ZERO = new GridPoint2(0, 4);
+  public static final GridPoint2 ZERO = new GridPoint2(0, 0);
 
   private GridPoint2Utils() {
     throw new IllegalStateException("Instantiating static util class");
