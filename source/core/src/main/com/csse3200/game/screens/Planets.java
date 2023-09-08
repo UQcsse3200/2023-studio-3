c5ef4d4f7583ef28feec95fc27e33d94498b38ce
Ryan McNeilly <ryanmcn@live.com.au>
2023-09-08 15:14:55 +1000

    Added planets class which encapsulates a planet object. Should be converted to a struct or enum of a sort.

--- source/core/src/main/com/csse3200/game/screens/Planets.java
+++ source/core/src/main/com/csse3200/game/screens/Planets.java
@@ -4,21 +4,32 @@ import com.badlogic.gdx.Gdx;
 
 public class Planets {
 
-    // Stores the posx and posy in form of constants for each planet
+    // Stores the posx and posy in form of constants for each planet and some form of ID.
     public final static int[] DESERT = {
-            (int) (Gdx.graphics.getWidth() / 6.0f),
-            (int) (Gdx.graphics.getHeight() / 2.3f)
+            (int) (Gdx.graphics.getWidth() / 6.0f),  // pos x
+            (int) (Gdx.graphics.getHeight() / 2.3f), // pos y
+            150,                                     // width
+            150,                                     // height
+            0                                        // ID
     };
 
     public final static int[] ICE = {
             (int) (Gdx.graphics.getWidth() / 3.0f),
-            (int) (Gdx.graphics.getHeight() / 1.4f)
+            (int) (Gdx.graphics.getHeight() / 1.4f),
+            150,
+            150,
+            1
     };
 
     public final static int[] LAVA = {
             (int) (Gdx.graphics.getWidth() / 2.2f),
-            (int) (Gdx.graphics.getHeight() / 7f)
+            (int) (Gdx.graphics.getHeight() / 7f),
+            200,
+            200,
+            2
     };
+
+    public final static int[][] PLANETS = {DESERT, ICE, LAVA};
 
 
 }
