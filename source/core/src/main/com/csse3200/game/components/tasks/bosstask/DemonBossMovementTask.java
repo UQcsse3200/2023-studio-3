f7940500c4dfa473c6e1572dad99dafc1803ebe6
gregchan550 <gregchan550@gmail.com>
2023-09-16 23:49:57 +1000

    Implemented jump function which utilises MovemenTask and PhysicsMovementComponent to get demon boss to jump to new position

--- source/core/src/main/com/csse3200/game/components/tasks/bosstask/DemonBossMovementTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/bosstask/DemonBossMovementTask.java
@@ -1,9 +1,13 @@
 package com.csse3200.game.components.tasks.bosstask;
 
 import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.ai.tasks.AITaskComponent;
 import com.csse3200.game.ai.tasks.DefaultTask;
 import com.csse3200.game.ai.tasks.PriorityTask;
 import com.csse3200.game.components.tasks.MovementTask;
+import com.csse3200.game.physics.PhysicsEngine;
+import com.csse3200.game.physics.components.PhysicsMovementComponent;
+import com.csse3200.game.services.ServiceLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +17,8 @@ public class DemonBossMovementTask extends DefaultTask implements PriorityTask {
     private static final int PRIORITY = 3;
     private Vector2 currentPos;
     private MovementTask movementTask;
+    private final PhysicsEngine physics;
+    private static final Vector2 DEMON_JUMP_SPEED = new Vector2(2f, 2f);
 
     private enum STATE {
         WALK, JUMP, IDLE
@@ -20,10 +26,29 @@ public class DemonBossMovementTask extends DefaultTask implements PriorityTask {
     private STATE demonState = STATE.IDLE;
 
     public DemonBossMovementTask() {
+        physics = ServiceLocator.getPhysicsService().getPhysics();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        this.currentPos = owner.getEntity().getPosition();
+    }
+
+    @Override
+    public void update() {
 
     }
+
     @Override
     public int getPriority() {
         return PRIORITY;
+    }
+
+    private void jump(Vector2 finalPos) {
+        MovementTask jump = new MovementTask(finalPos);
+        jump.create(owner);
+        owner.getEntity().getComponent(PhysicsMovementComponent.class).setSpeed(DEMON_JUMP_SPEED);
+        jump.start();
     }
 }
