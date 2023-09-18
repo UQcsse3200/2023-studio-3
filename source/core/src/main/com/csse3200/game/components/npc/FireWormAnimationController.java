3e0d12f8b2e72bc1d96d5c1850602ed156cc9f5e
MiniSoda17 <isaac.arli@gmail.com>
2023-09-18 21:04:35 +0700

    Added FireWormAnimationController and new fireworm assets

---
+++ source/core/src/main/com/csse3200/game/components/npc/FireWormAnimationController.java
@@ -0,0 +1,38 @@
+package com.csse3200.game.components.npc;
+
+import com.badlogic.gdx.audio.Sound;
+import com.csse3200.game.components.Component;
+import com.csse3200.game.rendering.AnimationRenderComponent;
+import com.csse3200.game.services.ServiceLocator;
+
+import java.util.Random;
+
+/**
+ * This class listens to events relevant to a ghost entity's state and plays the animation when one
+ * of the events is triggered.
+ */
+public class FireWormAnimationController extends Component {
+    // // For on collision sounds later
+    // private static final String COLLISION_SFX = "sounds/projectiles/on_collision.mp3";
+    // Sound onCollisionSound = ServiceLocator.getResourceService().getAsset(
+    //         COLLISION_SFX, Sound.class);
+    AnimationRenderComponent animator;
+    Random rand = new Random();
+
+    @Override
+    public void create() {
+        super.create();
+        animator = this.entity.getComponent(AnimationRenderComponent.class);
+        entity.getEvents().addListener("wanderStart", this::animateWalk);
+        entity.getEvents().addListener("shootStart", this::animateAttack);
+    }
+
+    void animateWalk() {
+        animator.startAnimation("fire_worm_walk");
+    }
+
+    void animateAttack() {
+        animator.startAnimation("xeno_shoot");
+    }
+}
+
