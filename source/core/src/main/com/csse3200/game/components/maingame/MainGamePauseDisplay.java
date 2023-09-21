7bf761f715b8d2504a0dd5d2d8c17c56ab8035c6
MajorDzaster <alasdair.sinclair@uqconnect.edu.au>
2023-09-21 20:39:37 +1000

    Added MainGamePauseDisplay as a component to the ui creation in MainGameScreen, creating a pause button in the top right of the game screen

---
+++ source/core/src/main/com/csse3200/game/components/maingame/MainGamePauseDisplay.java
@@ -0,0 +1,60 @@
+package com.csse3200.game.components.maingame;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.csse3200.game.ui.UIComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainGamePauseDisplay extends UIComponent {
+    private static final Logger logger = LoggerFactory.getLogger(MainGameExitDisplay.class);
+    private static final float Z_INDEX = 2f;
+    private Table table;
+
+    @Override
+    public void create() {
+        super.create();
+        addActors();
+    }
+
+    private void addActors() {
+        table = new Table();
+        table.top().right();
+        table.setFillParent(true);
+
+        TextButton mainMenuBtn = new TextButton("Pause", skin);
+
+        // Triggers an event when the button is pressed.
+        mainMenuBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Pause button clicked");
+                        entity.getEvents().trigger("pause");
+                    }
+                });
+
+        table.add(mainMenuBtn).padTop(100f).padRight(10f);
+
+        stage.addActor(table);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        // draw is handled by the stage
+    }
+
+    @Override
+    public float getZIndex() {
+        return Z_INDEX;
+    }
+
+    @Override
+    public void dispose() {
+        table.clear();
+        super.dispose();
+    }
+}
