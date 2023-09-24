3e30cff0bf21c01d02dd75d28d0332136856e783
gregchan550 <86044792+gregchan550@users.noreply.github.com>
2023-09-24 22:09:49 +1000

    removed cast state because its handled by teleport task. added melee attack method which deals damage to target and teleports back to original position

--- source/core/src/main/com/csse3200/game/components/tasks/bosstask/PatrickTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/bosstask/PatrickTask.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.ai.tasks.DefaultTask;
 import com.csse3200.game.ai.tasks.PriorityTask;
+import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.components.ProjectileEffects;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.factories.ProjectileFactory;
@@ -22,6 +23,7 @@ public class PatrickTask extends DefaultTask implements PriorityTask {
     private static final int PRIORITY = 3;
     private static final Vector2 PATRICK_SPEED = new Vector2(1f, 1f);
     private static final float MAX_RADIUS = 20f;
+    private static final int ATTACK_DAMAGE = 100;
 
     // Private variables
     private static final Logger logger = LoggerFactory.getLogger(PatrickTask.class);
@@ -35,12 +37,12 @@ public class PatrickTask extends DefaultTask implements PriorityTask {
     private int shotsFired;
     private PatrickTeleportTask teleportTask;
     private Vector2 initialPos;
+    private Entity meleeTarget;
 
     // Flags
     private boolean meleeFlag = false;
     private boolean rangeFlag = false;
     private boolean spawnFlag = false;
-    private boolean attackFlag = false;
     private  enum PatrickState {
         IDLE, WALK, ATTACK, HURT, DEATH, CAST, SPELL, APPEAR
     }
@@ -73,7 +75,8 @@ public class PatrickTask extends DefaultTask implements PriorityTask {
             case APPEAR -> {
                 if (animation.isFinished()) {
                     if (spawnFlag) {
-                        changeState(PatrickState.CAST);
+                        meleeAttack();
+                        meleeFlag = true;
                         spawnFlag = false;
                     } else if (meleeFlag) {
                         changeState(PatrickState.ATTACK);
@@ -81,16 +84,6 @@ public class PatrickTask extends DefaultTask implements PriorityTask {
                     } else if (rangeFlag) {
                         changeState(PatrickState.IDLE);
                         rangeFlag = false;
-                    }
-                }
-            }
-            case CAST -> {
-                if (animation.isFinished()) {
-                    if (attackFlag) {
-                        initialPos = patrick.getPosition();
-                        teleport(ServiceLocator.getEntityService().getClosestHuman(patrick).getPosition());
-                    } else {
-                        teleport(initialPos);
                     }
                 }
             }
@@ -108,6 +101,12 @@ public class PatrickTask extends DefaultTask implements PriorityTask {
                     ServiceLocator.getEntityService().register(projectile);
                     changeState(PatrickState.IDLE);
                     shotsFired++;
+                }
+            }
+            case ATTACK -> {
+                if (animation.isFinished()) {
+                    meleeTarget.getComponent(CombatStatsComponent.class).hit(ATTACK_DAMAGE);
+                    teleport(initialPos);
                 }
             }
         }
@@ -165,5 +164,11 @@ public class PatrickTask extends DefaultTask implements PriorityTask {
 
     private void teleport(Vector2 pos) {
         teleportTask = new PatrickTeleportTask(patrick, pos);
+    }
+
+    private void meleeAttack() {
+        initialPos = patrick.getPosition();
+        meleeTarget = ServiceLocator.getEntityService().getClosestHuman(patrick);
+        teleport(meleeTarget.getPosition());
     }
 }
