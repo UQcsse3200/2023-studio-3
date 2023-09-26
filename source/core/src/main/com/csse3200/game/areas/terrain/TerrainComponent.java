50b7c186f8a921efac205fcf556816c21c635503
Mohamad <moh.dabboussi@gmail.com>
2023-09-26 22:52:17 +1000

    Added colorTile

--- source/core/src/main/com/csse3200/game/areas/terrain/TerrainComponent.java
+++ source/core/src/main/com/csse3200/game/areas/terrain/TerrainComponent.java
@@ -1,13 +1,22 @@
 package com.csse3200.game.areas.terrain;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.csse3200.game.rendering.RenderComponent;
+import com.csse3200.game.services.ServiceLocator;
 
 /**
  * Render a tiled terrain for a given tiled map and orientation. A terrain is a map of tiles that
@@ -21,6 +30,7 @@ public class TerrainComponent extends RenderComponent {
   private final OrthographicCamera camera;
   private final TerrainOrientation orientation;
   private final float tileSize;
+  private  Texture blueTexture;
 
   public TerrainComponent(
       OrthographicCamera camera,
@@ -33,6 +43,14 @@ public class TerrainComponent extends RenderComponent {
     this.orientation = orientation;
     this.tileSize = tileSize;
     this.tiledMapRenderer = renderer;
+    Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+    pixmap.setColor(0, 0, 1, 1);  // Set to blue color (R=0, G=0, B=1, Alpha=1)
+    pixmap.fill();
+    blueTexture = new Texture(pixmap);
+
+// Remember to dispose of the Pixmap once you're done with it to free up memory
+    pixmap.dispose();
+
   }
 
   public Vector2 tileToWorldPosition(GridPoint2 tilePos) {
@@ -87,6 +105,30 @@ public class TerrainComponent extends RenderComponent {
   @Override
   public int getLayer() {
     return TERRAIN_LAYER;
+  }
+
+  public void colorTile(int x,int y) {
+    TiledMapTileLayer tile = (TiledMapTileLayer) tiledMap.getLayers().get(0);
+    TiledMapTile originalTile = tile.getCell(x,y).getTile();
+    StaticTiledMapTile blueTile = new StaticTiledMapTile(new TextureRegion(blueTexture));
+//    tile.getCell(x,y).setTile();
+    tile.getCell((int) x, (int) y).setTile(blueTile);
+
+    // Create an actor to handle the fade-in and fade-out effect
+    Actor fadeActor = new Actor();
+    fadeActor.addAction(Actions.sequence(
+            Actions.fadeIn(1f),
+            Actions.fadeOut(1f),
+            new Action() {
+              @Override
+              public boolean act(float delta) {
+                tile.getCell(x, y).setTile(originalTile);
+                return true;
+              }
+            }
+    ));
+    // Add this actor to the stage to process the actions
+    ServiceLocator.getRenderService().getStage().addActor(fadeActor);
   }
 
   public enum TerrainOrientation {
