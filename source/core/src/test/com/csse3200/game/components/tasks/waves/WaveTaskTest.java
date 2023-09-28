c1d2ad6715bd3a69a4dd571f709b9bfe490a3216
BlairCannon97 <blair.cannon@uq.net.au>
2023-09-29 09:07:32 +1000

    Added test directory 'waves' to test suite, and WaveTaskTest class

---
+++ source/core/src/test/com/csse3200/game/components/tasks/waves/WaveTaskTest.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.components.tasks.waves;
+
+import com.csse3200.game.extensions.GameExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
+class WaveTaskTest {
+
+}
