384e971f20aca0333bf49479b4b5c8856db897fa
Ahmad Abu-Aysha <111224176+The-AhmadAA@users.noreply.github.com>
2023-09-30 23:28:40 +1000

    deleted a whole bunch of commented out code in ForestGameArea, added missing assets to AssetLoader

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -43,142 +43,11 @@ public class ForestGameArea extends GameArea {
   private int wave = 0;
   private Timer waveTimer;
 
-  private Timer bossSpawnTimer;
-  private int bossSpawnInterval = 10000; // 1 minute in milliseconds
   private static final int NUM_WEAPON_TOWERS = 3;
   private static final GridPoint2 PLAYER_SPAWN = new GridPoint2(2, 4);
   // Temporary spawn point for testing
   private static final float WALL_WIDTH = 0.1f;
-  // Required to load assets before using them
-  private static final String[] forestTextures = {
-         "images/desert_bg.png",
-          "images/ice_bg.png",
-          "images/lava_bg.png",
-          "images/projectiles/projectile.png",
-          "images/ingamebg.png",
-          "images/box_boy_leaf.png",
-          "images/background/building1.png",
-          "images/ghost_1.png",
-          "images/grass_2.png",
-          "images/grass_3.png",
-          "images/hex_grass_1.png",
-          "images/background/mountain.png",
-          "images/ghost_king.png",
-          "images/ghost_1.png",
-          "images/terrain 2 normal.png",
-          "images/terrain 2 hex.png",
-          "images/hex_grass_2.png",
-          "images/hex_grass_3.png",
-          "images/iso_grass_1.png",
-          "images/iso_grass_2.png",
-          "images/iso_grass_3.png",
-          "images/towers/turret.png",
-          "images/towers/turret01.png",
-          "images/towers/turret_deployed.png",
-          "images/towers/fire_tower_atlas.png",
-          "images/towers/stun_tower.png",
-          "images/background/building2.png",
-          "images/mobs/robot.png",
-          "images/mobs/boss2.png",
-          "images/mobs/Attack_1.png",
-          "images/mobs/Attack_2.png",
-          "images/mobs/Charge_1.png",
-          "images/mobs/Charge_2.png",
-          "images/mobs/Dead.png",
-          "images/mobs/Enabling-5.png",
-          "images/mobs/satyr.png",
-          "images/mobs/Hurt.png",
-          "images/mobs/Idle.png",
-          "images/mobs/rangeBossRight.png",
-          "images/towers/wallTower.png",
-          "images/background/building2.png",
-          "images/iso_grass_3.png",
-          "images/terrain_use.png",
-          "images/Dusty_MoonBG.png",
-          "images/economy/scrap.png",
-          "images/economy/crystal.png",
-          "images/economy/econ-tower.png",
-          "images/projectiles/bossProjectile.png",
-          "images/towers/mine_tower.png",
-          "images/towers/TNTTower.png",
-          "images/towers/DroidTower.png",
-          "images/projectiles/basic_projectile.png",
-          "images/projectiles/mobProjectile.png",
-          "images/projectiles/engineer_projectile.png",
-          "images/projectiles/mobBoss_projectile.png",
-          "images/projectiles/snow_ball.png",
-          "images/projectiles/burn_effect.png",
-          "images/projectiles/stun_effect.png",
-          "images/projectiles/firework_anim.png",
-          "images/projectiles/pierce_anim.png",
-          "images/projectiles/snow_ball.png",
-          "images/mobboss/demon.png",
-          "images/mobboss/demon2.png",
-          "images/mobs/fire_worm.png",
-          "images/mobboss/patrick.png",
-          "images/GrassTile/grass_tile_1.png",
-          "images/GrassTile/grass_tile_2.png",
-          "images/GrassTile/grass_tile_3.png",
-          "images/GrassTile/grass_tile_4.png",
-          "images/GrassTile/grass_tile_5.png",
-          "images/GrassTile/grass_tile_6.png",
-          "images/GrassTile/grass_tile_7.png",
-          "images/highlight_tile.png"
-  };
-  private static final String[] forestTextureAtlases = {
-          "images/economy/econ-tower.atlas",
-          "images/terrain_iso_grass.atlas",
-          "images/ghost.atlas",
-          "images/mobs/boss2.atlas",
-          "images/ghostKing.atlas",
-          "images/towers/turret.atlas",
-          "images/towers/turret01.atlas",
-          "images/mobs/xenoGrunt.atlas",
-          "images/towers/fire_tower_atlas.atlas",
-          "images/towers/stun_tower.atlas",
-          "images/mobs/xenoGruntRunning.atlas",
-          "images/xenoGrunt.atlas",
-          "images/mobs/robot.atlas",
-          "images/mobs/rangeBossRight.atlas",
-          "images/towers/DroidTower.atlas",
-          "images/mobs/robot.atlas",
-          "images/mobs/rangeBossRight.atlas",
-          "images/towers/TNTTower.atlas",
-          "images/projectiles/basic_projectile.atlas",
-          "images/projectiles/bossProjectile.atlas",
-          "images/projectiles/mobProjectile.atlas",
-          "images/projectiles/mobProjectile.atlas",
-          "images/projectiles/engineer_projectile.atlas",
-          "images/projectiles/mobBoss_projectile.atlas",
-          "images/projectiles/snow_ball.atlas",
-          "images/projectiles/pierce_anim.atlas",
-          "images/projectiles/burn_effect.atlas",
-          "images/projectiles/firework_anim.atlas",
-          "images/projectiles/mobProjectile.atlas",
-          "images/projectiles/stun_effect.atlas",
-          "images/mobboss/demon.atlas",
-          "images/mobs/fire_worm.atlas",
-          "images/mobs/dragon_knight.atlas",
-          "images/mobs/skeleton.atlas",
-          "images/mobs/wizard.atlas", 
-          "images/mobs/water_queen.atlas",
-          "images/mobs/water_slime.atlas",
-          "images/mobboss/patrick.atlas"
-  };
-  private static final String[] forestSounds = {
-          "sounds/Impact4.ogg",
-          "sounds/economy/click.wav",
-          "sounds/economy/click_1.wav",
-          "sounds/towers/gun_shot_trimmed.mp3",
-          "sounds/towers/deploy.mp3",
-          "sounds/towers/stow.mp3",
-          "sounds/engineers/firing_auto.mp3",
-          "sounds/engineers/firing_single.mp3",
-          "sounds/projectiles/on_collision.mp3",
-          "sounds/projectiles/explosion.mp3"
-  };
-  private static final String backgroundMusic = "sounds/background/Sci-Fi1.ogg";
-  private static final String[] forestMusic = {backgroundMusic};
+
   private final TerrainFactory terrainFactory;
   
   private Entity player;
@@ -249,7 +118,6 @@ public class ForestGameArea extends GameArea {
         break;
     }
   }
-
 
   /**
    * Create the game area, including terrain, static entities (trees), dynamic entities (player)
@@ -328,48 +196,6 @@ public class ForestGameArea extends GameArea {
             false,
             false);
   }
-
-
-//  private void spawnBuilding1() {
-//    GridPoint2 minPos = new GridPoint2(0, 0);
-//    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 2);
-//
-//    for (int i = 0; i < NUM_BUILDINGS; i++) {
-//      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
-//      Entity building1 = ObstacleFactory.createBuilding1();
-//      spawnEntityAt(building1, randomPos, true, false);
-//    }
-//  }
-//  private void spawnBuilding2() {
-//    GridPoint2 minPos = new GridPoint2(0, 0);
-//    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 2);
-//
-//    for (int i = 0; i < NUM_BUILDINGS; i++) {
-//      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
-//      Entity building2 = ObstacleFactory.createBuilding2();
-//      spawnEntityAt(building2, randomPos, true, false);
-//    }
-//  }
-
-
-//  private void spawnMountains() {
-//    ArrayList<GridPoint2> fixedPositions = new ArrayList<>(); //Generating ArrayList
-//
-//    fixedPositions.add(new GridPoint2(5, 8));
-//    fixedPositions.add(new GridPoint2(12, 4));
-//    fixedPositions.add(new GridPoint2(20, 10));
-//    fixedPositions.add(new GridPoint2(33, 17));
-//
-//    for (GridPoint2 fixedPos : fixedPositions) {
-//      Entity tree = ObstacleFactory.createMountain();
-//      spawnEntityAt(tree, fixedPos, true, false);
-//    }
-//    for (int i = 0; i < NUM_BUILDINGS; i++) {
-//      GridPoint2 randomPos = RandomUtils.random(minPos, maxPos);
-//      Entity building1 = ObstacleFactory.createBuilding1();
-//      spawnEntityAt(building1, randomPos, true, false);
-//    }
-//  }
   
   private void spawnBuilding2() {
     GridPoint2 minPos = new GridPoint2(0, 0);
@@ -395,29 +221,6 @@ public class ForestGameArea extends GameArea {
     return newPlayer;
   }
 
-//  private void spawnGhosts() {
-//    GridPoint2 minPos = new GridPoint2(0, 0);
-//    GridPoint2 maxPos = terrain.getMapBounds(0).sub(0, 2);
-//
-//    for (int i = 0; i < NUM_GHOSTS; i++) {
-//      int fixedX = terrain.getMapBounds(0).x - 1; // Rightmost x-coordinate
-//      int randomY = MathUtils.random(0, maxPos.y);
-//      GridPoint2 randomPos = new GridPoint2(fixedX, randomY);
-//      Entity ghost = createGhost(player);
-//      spawnEntityAt(ghost, randomPos, true, true);
-//    }
-//  }
-
-//  private Entity spawnMobBoss1() {
-//    GridPoint2 minPos = new GridPoint2(0, 0);
-//    GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 2);
-//    GridPoint2 randomPos
-//     = new GridPoint2(0, 0);
-//    Entity ghostKing = NPCFactory.createGhostKing(player);
-//    spawnEntityAt(ghostKing, randomPos, true, true);
-//    return ghostKing;
-//  }
-
   private void spawnDemonBoss() {
     Entity demon = MobBossFactory.createDemonBoss();
     spawnEntityAt(demon, new GridPoint2(19, 5), true, false);
@@ -441,7 +244,6 @@ public class ForestGameArea extends GameArea {
     }
     return mobBoss1;
   }
-
 
   /**
    * Spawns a projectile that only heads towards the enemies in its lane.
@@ -586,18 +388,6 @@ public class ForestGameArea extends GameArea {
       spawnEntityAt(waterSlime, randomPos, true, false);
     }
   }
-
-//  private Entity spawnGhostKing() {
-//    GridPoint2 minPos = new GridPoint2(0, 0);
-//    GridPoint2 maxPos = terrain.getMapBounds(0).sub(0, 0);
-//    GridPoint2 randomPos
-//            = RandomUtils.random(minPos, maxPos);
-//    // = new GridPoint2(26, 26);
-//    Entity ghostKing = NPCFactory.createGhostKing(player);
-//    spawnEntityAt(ghostKing, randomPos, true, true);
-//    return ghostKing;
-//
-//  }
   
   private void spawnWeaponTower() {
     GridPoint2 minPos = new GridPoint2(0, 2);
@@ -615,7 +405,7 @@ public class ForestGameArea extends GameArea {
   }
 
   // * TEMPORARY FOR TESTING
-  private void spawnFireTowerTowerAt(int x, int y) {
+  private void spawnFireTowerAt(int x, int y) {
     GridPoint2 pos = new GridPoint2(x, y);
     Entity fireTower = TowerFactory.createFireTower();
 
@@ -650,16 +440,6 @@ public class ForestGameArea extends GameArea {
       spawnEntityAt(weaponTower, randomPos, true, true);
     }
   }
-  
-  
-//  private void playMusic() {
-//    Music music = ServiceLocator.getResourceService().getAsset(backgroundMusic, Music.class);
-//    music.setLooping(true);
-//    music.setVolume(0.3f);
-//    music.play();
-//  }
-  
-
 
   private void unloadAssets() {
     logger.debug("Unloading assets");
@@ -673,7 +453,6 @@ public class ForestGameArea extends GameArea {
   @Override
   public void dispose() {
     super.dispose();
-//    ServiceLocator.getResourceService().getAsset(backgroundMusic, Music.class).stop();
     this.unloadAssets();
     stopWaveTimer();
   }
