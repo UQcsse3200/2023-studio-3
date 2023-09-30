6e056bc4a84698e44e2c7487e1168da99ad77b25
BlairCannon97 <blair.cannon@uq.net.au>
2023-09-30 11:37:53 +1000

    Added Test classes WaveFactoryTest, LevelWavesTest and WaveClassTest to aid in testing of the waves feature.

---
+++ source/core/src/test/com/csse3200/game/components/tasks/waves/LevelWavesTest.java
@@ -0,0 +1,21 @@
+package com.csse3200.game.components.tasks.waves;
+
+import com.csse3200.game.areas.ForestGameArea;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.services.GameTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
+class LevelWavesTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}
--- source/core/src/test/com/csse3200/game/components/tasks/waves/WaveTaskTest.java
+++ source/core/src/test/com/csse3200/game/components/tasks/waves/WaveTaskTest.java
@@ -15,6 +15,7 @@ import java.util.logging.Level;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(GameExtension.class)
 @ExtendWith(MockitoExtension.class)
