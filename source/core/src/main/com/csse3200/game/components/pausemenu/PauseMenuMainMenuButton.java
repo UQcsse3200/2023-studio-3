01f3724195a391ffc36a3c603f916238ddea43c0
MajorDzaster <alasdair.sinclair@uqconnect.edu.au>
2023-10-02 17:20:19 +1000

    Improved sprites for pause menu buttons.

--- source/core/src/main/com/csse3200/game/components/pausemenu/PauseMenuMainMenuButton.java
+++ source/core/src/main/com/csse3200/game/components/pausemenu/PauseMenuMainMenuButton.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.csse3200.game.GdxGame;
+import com.csse3200.game.ui.ButtonFactory;
 import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,8 +36,8 @@ public class PauseMenuMainMenuButton extends UIComponent {
         table = new Table();
         table.top().right();
         table.setFillParent(true);
-
-        TextButton pauseMenuBtn = new TextButton("Main Menu", skin);
+        ButtonFactory buttonFactory = new ButtonFactory();
+        TextButton pauseMenuBtn = buttonFactory.createButton("Main Menu");
 
         // Triggers an event when the button is pressed.
         pauseMenuBtn.addListener(
