fe1be661ec8af42aae8b8a2cf4de90fe87d22fd7
Kevin <104761532+Hasakev@users.noreply.github.com>
2023-10-02 13:19:15 +1000

    Made changes to UI upgrade box

--- source/core/src/main/com/csse3200/game/input/UpgradeUIComponent.java
+++ source/core/src/main/com/csse3200/game/input/UpgradeUIComponent.java
@@ -9,13 +9,13 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Align;
 import com.csse3200.game.areas.ForestGameArea;
-import com.csse3200.game.components.gamearea.TowerUpgradeDisplay;
+import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.components.tower.TowerUpgraderComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.EntityService;
@@ -29,6 +29,8 @@ public class UpgradeUIComponent extends InputComponent {
     private final Camera camera;
     private final Stage stage;
 
+
+
     private Table upgradeTable;
     int value;
 
@@ -41,6 +43,8 @@ public class UpgradeUIComponent extends InputComponent {
         this.entityService = ServiceLocator.getEntityService();
         this.camera = camera;
         this.stage = stage;
+
+
     }
 
     /**
@@ -73,32 +77,64 @@ public class UpgradeUIComponent extends InputComponent {
 
         if (clickedEntity != null && clickedEntity.getComponent(TowerUpgraderComponent.class) != null) {
             logger.info("clicked a turret that is upgradable!");
-            createUpgradeTable(clickedEntity, screenX, screenY);
+            createUpgradeTable(clickedEntity);
+            Vector2 UICoordinates = stage.screenToStageCoordinates(new Vector2(screenX, screenY));
+            upgradeTable.setPosition(UICoordinates.x, UICoordinates.y);
             stage.addActor(upgradeTable);
             return true;
         }
         return false;
     }
 
-    private void createUpgradeTable(Entity turretEntity, int screenX, int screenY) {
+    private void createUpgradeTable(Entity turretEntity) {
         upgradeTable = new Table();
-        upgradeTable.align(Align.top);
-        upgradeTable.setPosition(screenX, screenY);
-
-        // Create a BitmapFont for the LabelStyle
-        BitmapFont font = new BitmapFont(); // You can customize this font as needed
+        upgradeTable.top();
+        upgradeTable.defaults().pad(0).space(0);
+        upgradeTable.setSize(100,100);
+        Table innerUpgradeTable = new Table();
+        innerUpgradeTable.top();
+        innerUpgradeTable.defaults().pad(0).space(0);
+        innerUpgradeTable.setSize(100,100);
+        // set table background
+        String imageFilePath = "images/ui/Sprites/UI_Glass_Frame_Standard_01a.png";
+        String upgradeButtonFilePath = "images/economy/scrapBanner.png";
+        Drawable drawableBackground = new TextureRegionDrawable(new TextureRegion(new Texture(imageFilePath)));
+        innerUpgradeTable.setBackground(drawableBackground);
+//        upgradeTable.setBackground(drawableBackground);
 
         Drawable drawable = new TextureRegionDrawable(new TextureRegion(new Texture("images/ui/Sprites/UI_Glass_Button_Small_Lock_01a2.png")));
+        Drawable econDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(upgradeButtonFilePath)));
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle(
                 drawable, drawable, drawable, new BitmapFont());
-
+        TextButton.TextButtonStyle econStyle = new TextButton.TextButtonStyle(
+                econDrawable, econDrawable, econDrawable, new BitmapFont());
         // create button
-        TextButton tb = new TextButton("Hello World", style);
-        tb.setDisabled(true);
+        int maxHealth = turretEntity.getComponent(CombatStatsComponent.class).getMaxHealth();
+        Label health = new Label(String.format("Max Health: %d", maxHealth), createLabelStyle());
+        Label attack = new Label(String.format("Attack: %d", turretEntity.getComponent(CombatStatsComponent.class).getBaseAttack()), createLabelStyle());
+        TextButton closeButton = new TextButton("X", style);
+        TextButton upgradeButton = new TextButton("Upgrade", style);
+        upgradeTable.add(closeButton).right().top();
+        upgradeTable.row();
 
-        upgradeTable.add(tb);
+        innerUpgradeTable.row();
+        innerUpgradeTable.add(health);
+        innerUpgradeTable.row();
+        innerUpgradeTable.add(attack);
+        innerUpgradeTable.row();
+        innerUpgradeTable.add(upgradeButton);
+        upgradeTable.add(innerUpgradeTable).center().expand().row();
+
 
         upgradeTable.setVisible(true);
+    }
+
+    private LabelStyle createLabelStyle() {
+        LabelStyle style = new LabelStyle();
+
+        style.font = new BitmapFont();
+        style.fontColor = Color.WHITE;
+        return style;
     }
 
 
