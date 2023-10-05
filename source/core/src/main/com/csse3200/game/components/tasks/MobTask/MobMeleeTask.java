19d1eaa0360e5f7cd7644f96d2731511071c4050
gregchan550 <gregchan550@gmail.com>
2023-10-05 21:26:08 +1000

    created new mob melee and ranged tasks as well as enum for general mob types

--- source/core/src/main/com/csse3200/game/components/tasks/MobTask/MobMeleeTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/MobTask/MobMeleeTask.java
@@ -1,4 +1,48 @@
 package com.csse3200.game.components.tasks.MobTask;
 
-public class MobMeleeTask {
+import com.badlogic.gdx.utils.Timer;
+import com.csse3200.game.ai.tasks.DefaultTask;
+import com.csse3200.game.ai.tasks.PriorityTask;
+import com.csse3200.game.components.tasks.bosstask.PatrickTask;
+import com.csse3200.game.physics.components.PhysicsMovementComponent;
+import com.csse3200.game.rendering.AnimationRenderComponent;
+
+public class MobMeleeTask extends DefaultTask implements PriorityTask {
+
+    // Constants
+    private static final int PRIORITY = 3;
+    MobType mobType;
+    State state;
+    private enum State {
+        RUN, ATTACK, DEATH, DEFAULT
+    }
+
+    public MobMeleeTask(MobType mobType) {
+        this.mobType = mobType;
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    private void animate() {
+        switch (mobType) {
+            case SKELETON -> {
+                switch (state) {
+
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getPriority() {
+        return PRIORITY;
+    }
 }
---
+++ source/core/src/main/com/csse3200/game/components/tasks/MobTask/MobType.java
@@ -0,0 +1,10 @@
+package com.csse3200.game.components.tasks.MobTask;
+
+public enum MobType {
+    SKELETON,
+    WIZARD,
+    WATER_QUEEN,
+    WATER_SLIME,
+    FIRE_WORM,
+    DRAGON_KNIGHT
+}
