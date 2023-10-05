394c4cbe7b39a275582837ec5cac0394365a3eeb
gregchan550 <gregchan550@gmail.com>
2023-10-05 21:32:06 +1000

    implemented animate method which calls every general mob animation

--- source/core/src/main/com/csse3200/game/components/tasks/MobTask/MobMeleeTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/MobTask/MobMeleeTask.java
@@ -35,7 +35,50 @@ public class MobMeleeTask extends DefaultTask implements PriorityTask {
         switch (mobType) {
             case SKELETON -> {
                 switch (state) {
-
+                    case RUN -> owner.getEntity().getEvents().trigger("skeleton_walk");
+                    case ATTACK -> owner.getEntity().getEvents().trigger("skeleton_attack");
+                    case DEATH -> owner.getEntity().getEvents().trigger("skeleton_death");
+                    case DEFAULT -> owner.getEntity().getEvents().trigger("skeleton_default");
+                }
+            }
+            case WIZARD -> {
+                switch (state) {
+                    case RUN -> owner.getEntity().getEvents().trigger("wizard_run");
+                    case ATTACK -> owner.getEntity().getEvents().trigger("wizard_attack");
+                    case DEATH -> owner.getEntity().getEvents().trigger("wizard_death");
+                    case DEFAULT -> owner.getEntity().getEvents().trigger("default");
+                }
+            }
+            case WATER_QUEEN -> {
+                switch (state) {
+                    case RUN -> owner.getEntity().getEvents().trigger("water_queen_walk");
+                    case ATTACK -> owner.getEntity().getEvents().trigger("water_queen_attack");
+                    case DEATH -> owner.getEntity().getEvents().trigger("water_queen_death");
+                    case DEFAULT -> owner.getEntity().getEvents().trigger("default");
+                }
+            }
+            case WATER_SLIME -> {
+                switch (state) {
+                    case RUN -> owner.getEntity().getEvents().trigger("water_slime_walk");
+                    case ATTACK -> owner.getEntity().getEvents().trigger("water_slime_attack");
+                    case DEATH -> owner.getEntity().getEvents().trigger("water_slime_death");
+                    case DEFAULT -> owner.getEntity().getEvents().trigger("default");
+                }
+            }
+            case FIRE_WORM -> {
+                switch (state) {
+                    case RUN -> owner.getEntity().getEvents().trigger("fire_worm_walk");
+                    case ATTACK -> owner.getEntity().getEvents().trigger("fire_worm_attack");
+                    case DEATH -> owner.getEntity().getEvents().trigger("fire_worm_death");
+                    case DEFAULT -> owner.getEntity().getEvents().trigger("default");
+                }
+            }
+            case DRAGON_KNIGHT -> {
+                switch (state) {
+                    case RUN -> owner.getEntity().getEvents().trigger("dragon_knight_run");
+                    case ATTACK -> owner.getEntity().getEvents().trigger("dragon_knight_attack");
+                    case DEATH -> owner.getEntity().getEvents().trigger("dragon_knight_death");
+                    case DEFAULT -> owner.getEntity().getEvents().trigger("default");
                 }
             }
         }
