1e2ceadfcca45b2f3193e3cf74de992b4fb58444
Ahmad Abu-Aysha <111224176+The-AhmadAA@users.noreply.github.com>
2023-10-14 13:14:35 +1000

    ButtonFactory updated with current skin settings for universal use

---
+++ source/core/src/main/com/csse3200/game/components/pausemenu/PauseMenuButtonComponent.java
@@ -0,0 +1,116 @@
+package com.csse3200.game.components.pausemenu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.ui.UIComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PauseMenuButtonComponent extends UIComponent {
+    private static final Logger logger = LoggerFactory.getLogger(PauseMenuButtonComponent.class);
+    private static final float Z_INDEX = 2f;
+    private Window window;
+    private final GdxGame game;
+
+    public PauseMenuButtonComponent(GdxGame screenSwitchHandle) {
+        game = screenSwitchHandle;
+    }
+
+    @Override
+    public void create() {
+        super.create();
+        addActors();
+    }
+
+    /**
+     * Initialises the pause menu buttons
+     * Positions them on the stage using a table
+     */
+    private void addActors() {
+        //window = new Window("",);
+        window = new Window("Game Paused", new Skin(Gdx.files.internal("images/ui/buttons/glass.json")));
+
+        TextButton continueBtn = new TextButton("Continue", skin);
+        TextButton settingsBtn = new TextButton("Settings", skin);
+        TextButton planetSelectBtn = new TextButton("Planet Select", skin);
+        TextButton mainMenuBtn = new TextButton("Main Menu", skin);
+        continueBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Continue button clicked");
+                        entity.dispose();
+                    }
+                });
+        settingsBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Settings button clicked");
+                        game.setScreen(GdxGame.ScreenType.SETTINGS);
+                    }
+                });
+        planetSelectBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Planet select button clicked");
+                        game.setScreen(GdxGame.ScreenType.LEVEL_SELECT);
+                    }
+                });
+        mainMenuBtn.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent changeEvent, Actor actor) {
+                        logger.debug("Main menu button clicked");
+                        game.setScreen(GdxGame.ScreenType.MAIN_MENU);
+                    }
+                });
+
+        window.setKeepWithinStage(true);
+        window.setResizable(true);
+        window.setModal(true);
+        window.setTouchable(Touchable.enabled);
+        window.setMovable(true);
+        window.setFillParent(false);
+        window.center();
+        window.add(continueBtn).center();
+        window.row();
+        window.add(settingsBtn).center();
+        window.row();
+        window.add(planetSelectBtn).center();
+        window.row();
+        window.add(mainMenuBtn).center();
+        window.setHeight(300f);
+        window.setWidth(250f);
+        window.setX((ServiceLocator.getRenderService().getStage().getWidth() / 2) - 125f);
+        window.setY((ServiceLocator.getRenderService().getStage().getHeight() / 2) - 150f);
+
+        stage.addActor(window);
+    }
+
+    @Override
+    protected void draw(SpriteBatch batch) {
+        // handled by stage
+    }
+
+    @Override
+    public float getZIndex() {
+        return Z_INDEX;
+    }
+
+    @Override
+    public void dispose() {
+        window.clear();
+        window.remove();
+        super.dispose();
+    }
+}
--- source/core/src/main/com/csse3200/game/ui/ButtonFactory.java
+++ source/core/src/main/com/csse3200/game/ui/ButtonFactory.java
@@ -3,10 +3,11 @@ package com.csse3200.game.ui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import static com.csse3200.game.ui.UIComponent.skin;
 
 /**
  * This class provides static methods for creating various types of TextButtons with different styles.
@@ -15,21 +16,15 @@ public class ButtonFactory {
     private static Skin defaultSkin;
 
     // Static initializer block to initialize the default skin
-    static {
-        defaultSkin = createDefaultSkin();
-    }
+    // This is handled by UIComponent
+//    static {
+//        defaultSkin = new
+//    }
 
-    private static Skin createDefaultSkin() {
-        Skin skin = new Skin(Gdx.files.internal("configs/text.json"));
-
-        // Define the button style with the background image
-        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
-        style.font = skin.getFont("default");
-        style.up = new TextureRegionDrawable(new TextureRegion(new Texture("images/ui/Sprites/UI_Glass_Button_Large_Lock_01a1.png"))); // Set the button background to the loaded image
-
-        skin.add("default", style);
-        return skin;
-    }
+//    private static Skin createDefaultSkin() {
+//        Skin skin = new Skin(Gdx.files.internal("images/ui/buttons/glass.json"));
+//        return skin;
+//    }
 
     /**
      * Creates a TextButton with the specified text using the default skin.
@@ -38,9 +33,9 @@ public class ButtonFactory {
      * @return The created TextButton.
      */
     public static TextButton createButton(String text) {
-        TextButton button = new TextButton(text, defaultSkin);
-        button.getLabel().setFontScale(0.8f); // Adjust text size
-        button.pad(10f); // Adjust padding
+        TextButton button = new TextButton(text, skin);
+//        button.getLabel().setFontScale(0.8f); // Adjust text size
+        button.pad(20f); // Adjust padding
         return button;
     }
 
