49551bc6125c322a9fa950f09f5a51fe093d0c60
Ahmad Abu-Aysha <111224176+The-AhmadAA@users.noreply.github.com>
2023-10-14 15:40:25 +1000

    Update PauseMenuButtonComponent.java
    removed second declaration of GdxGame

--- source/core/src/main/com/csse3200/game/components/pausemenu/PauseMenuButtonComponent.java
+++ source/core/src/main/com/csse3200/game/components/pausemenu/PauseMenuButtonComponent.java
@@ -24,7 +24,6 @@ public class PauseMenuButtonComponent extends UIComponent {
     private static final float windowSizeY = 400;
 //    private static float padScaleFactorY;
 //    private static float padScaleFactorX;
-    private final GdxGame game;
 
     public PauseMenuButtonComponent(GdxGame screenSwitchHandle) {
         game = screenSwitchHandle;
