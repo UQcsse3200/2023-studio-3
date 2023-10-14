7e808400f9a098016f9301b4bb133d9aa63bfb50
Moksh Mehta <moksh.mehta@uq.net.au>
2023-10-15 05:11:23 +1100

    Added Background to Next Level Screen

--- source/core/src/main/com/csse3200/game/screens/NextLevelScreen.java
+++ source/core/src/main/com/csse3200/game/screens/NextLevelScreen.java
@@ -2,6 +2,7 @@ package com.csse3200.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -23,7 +24,7 @@ public class NextLevelScreen extends ScreenAdapter {
 
     public NextLevelScreen(GdxGame game) {
         batch = new SpriteBatch();
-        backgroundTexture = new Texture("planets/background.png"); // Replace with the path to your background image
+        backgroundTexture = new Texture("images/ui/Screen/Nextlevel.png"); // Replace with the path to your background image
         font = new BitmapFont();
 
         stage = new Stage(new ScreenViewport());
@@ -34,12 +35,9 @@ public class NextLevelScreen extends ScreenAdapter {
         nextLevelButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // Logic to move to the next level
-                // You can implement your logic here, such as updating the level and calling game.setScreen()
                 // Example: game.setScreen(new MainGameScreen(game, nextLevel));
             }
         });
-
         TextButton mainMenuButton = new TextButton("Main Menu", skin);
         mainMenuButton.addListener(new ClickListener() {
             @Override
@@ -50,8 +48,8 @@ public class NextLevelScreen extends ScreenAdapter {
 
         Table table = new Table();
         table.setFillParent(true);
-        table.add(nextLevelButton).padTop(-100).row();
-        table.add(mainMenuButton).padTop(-200).row();
+        table.add(nextLevelButton).padTop(400).row();
+        table.add(mainMenuButton).padTop(-150).row();
         stage.addActor(table);
     }
 
@@ -62,12 +60,25 @@ public class NextLevelScreen extends ScreenAdapter {
 
         batch.begin();
         batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        font.draw(batch, "Congratulations on completing the level!", 200, 400); // Customize the message and position
+
+        // Customize font size, color, and style
+        BitmapFont font = new BitmapFont();
+        font.getData().setScale(3.0f); // Set font size to 2x the default size
+        Color textColor = new Color(200 / 255f, 246 / 255f, 244 / 255f, 1f); // Convert RGB values to 0-1 range
+        font.setColor(textColor);
+
+        // GlyphLayout layout = new GlyphLayout(font, "Congratulations on completing the level!");
+        // float textX = (Gdx.graphics.getWidth() - layout.width) / 2; // Center the text horizontally
+        // float textY = 750; // Customize the vertical position
+
+        //  font.draw(batch, layout, textX, textY);
+
         batch.end();
 
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
     }
+
 
     @Override
     public void resize(int width, int height) {
@@ -81,3 +92,4 @@ public class NextLevelScreen extends ScreenAdapter {
         stage.dispose();
     }
 }
+
