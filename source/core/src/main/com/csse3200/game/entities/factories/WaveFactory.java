80915ea38c785e7312d9dee1177b0120329b6f52
MiniSoda17 <isaac.arli@gmail.com>
2023-10-16 23:47:30 +1000

    Fixing waves structure for Junit testing

--- source/core/src/main/com/csse3200/game/entities/factories/WaveFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/WaveFactory.java
@@ -41,10 +41,10 @@ public class WaveFactory {
 
   private static final ArrayList<ArrayList<String>> lvl2Structure = new ArrayList<>(Arrays.asList(
     new ArrayList<>(Arrays.asList("Skeleton"
-    )), new ArrayList<>(Arrays.asList("Skeleton", "Wizard"
+    )), new ArrayList<>(Arrays.asList("Skeleton", "ArcaneArcher"
       )), new ArrayList<>(Arrays.asList("Skeleton", "Wizard"
       )), new ArrayList<>(Arrays.asList("Skeleton", "SplittingNightBorne"
-      )), new ArrayList<>(Arrays.asList("ArcaneArcher", "SplittingNightBorne"
+      )), new ArrayList<>(Arrays.asList("Wizard", "SplittingNightBorne"
       )), new ArrayList<>(Arrays.asList("SplittingNightBorne", "Skeleton"
       )), new ArrayList<>(Arrays.asList("Wizard", "SplittingNightBorne"
       )), new ArrayList<>(Arrays.asList("ArcaneArcher", "SplittingNightBorne", "Wizard"
