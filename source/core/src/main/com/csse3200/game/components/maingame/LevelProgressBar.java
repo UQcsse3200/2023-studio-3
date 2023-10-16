f7e239f8c5b074e6ba9dcfa581913f174f0b1d94
Mohamad <moh.dabboussi@gmail.com>
2023-10-16 17:11:07 +1000

    Integrated Mob Count with Level Progress Bar

--- source/core/src/main/com/csse3200/game/components/maingame/LevelProgressBar.java
+++ source/core/src/main/com/csse3200/game/components/maingame/LevelProgressBar.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.components.maingame;
 
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
@@ -7,17 +8,20 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.csse3200.game.screens.GameLevelData;
+import com.csse3200.game.services.ServiceLocator;
 
 public class LevelProgressBar extends ProgressBar {
+
+    static int selectedLevel = GameLevelData.getSelectedLevel();
 
     /**
      * @param width of the health bar
      * @param height of the health bar
      */
     public LevelProgressBar(int width, int height) {
-        super(0f, 100f, 0.01f, false, new ProgressBarStyle());
+        super(0f, getMobCount(), 0.01f, false, new ProgressBarStyle());
         getStyle().background = getColoredDrawable(width, height, Color.RED);
-//        getStyle().knob = getColoredDrawable(0, height, Color.GREEN);
         getStyle().knob = new TextureRegionDrawable(new TextureRegion(new Texture("images/skeleton.png")));
         getStyle().knobBefore = getColoredDrawable(width, height, Color.GREEN);
 
@@ -30,6 +34,13 @@ public class LevelProgressBar extends ProgressBar {
         setAnimateDuration(0.25f);
     }
 
+    /**
+     * Color filling for the Progress Bar
+     * @param width the width in pixels
+     * @param height the height in pixels
+     * @param color the color of the filling
+     * @return Drawable
+     */
     public static Drawable getColoredDrawable(int width, int height, Color color) {
         Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
         pixmap.setColor(color);
@@ -40,5 +51,28 @@ public class LevelProgressBar extends ProgressBar {
         pixmap.dispose();
 
         return drawable;
+    }
+
+    /**
+     * Get the number of mobs based on the level
+     * @return number of total mobs
+     */
+    public static int getMobCount() {
+
+        switch (selectedLevel) {
+            // Desert
+            case 1 -> { // Ice
+                ServiceLocator.getWaveService().setTotalMobs(91);
+                return 91;
+            }
+            case 2 -> { // Lava
+                ServiceLocator.getWaveService().setTotalMobs(204);
+                return 204;
+            }
+            default -> {
+                ServiceLocator.getWaveService().setTotalMobs(27);
+                return 27;
+            }
+        }
     }
 }
