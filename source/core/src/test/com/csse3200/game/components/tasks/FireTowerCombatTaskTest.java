bf9994615510876cba6992396e781f0c24636ea2
Nhat Minh Le <lenhatminh451@gmail.com>
2023-10-17 00:45:18 +1000

    Fix code smells in remaining task tests + BombshipFactory

--- source/core/src/test/com/csse3200/game/components/tasks/FireTowerCombatTaskTest.java
+++ source/core/src/test/com/csse3200/game/components/tasks/FireTowerCombatTaskTest.java
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class FireTowerCombatTaskTest {
+class FireTowerCombatTaskTest {
     FireTowerCombatTask fireTowerCombatTask;
 
     @BeforeEach
@@ -39,7 +39,7 @@ public class FireTowerCombatTaskTest {
      * in DroidCombatTaskTest by Mohamad Dabboussi
      */
     @Test
-    public void testStartTriggersIdleEvent() {
+    void testStartTriggersIdleEvent() {
         Entity entity = createFireTower();
         EventListener0 idleListener = mock(EventListener0.class);
         // Deploy Droid in the walking state
@@ -53,7 +53,7 @@ public class FireTowerCombatTaskTest {
      * in DroidCombatTaskTest by Mohamad Dabboussi
      */
     @Test
-    public void testUpdateTowerStateWithTargetInRange() {
+    void testUpdateTowerStateWithTargetInRange() {
         Entity entity = createFireTower();
         entity.setPosition(10, 10);
 
@@ -91,7 +91,7 @@ public class FireTowerCombatTaskTest {
      * in DroidCombatTaskTest by Mohamad Dabboussi
      */
     @Test
-    public void testUpdateTowerStateWithTargetNotInRange() {
+    void testUpdateTowerStateWithTargetNotInRange() {
         Entity entity = createFireTower();
         entity.setPosition(10, 10);
 
