d63461b58f30035483e55a151026ed9c52ca462d
Mohamad <moh.dabboussi@gmail.com>
2023-10-17 10:31:41 +1000

    Added Shake screen effect at every wave

--- source/core/src/main/com/csse3200/game/components/maingame/UIElementsDisplay.java
+++ source/core/src/main/com/csse3200/game/components/maingame/UIElementsDisplay.java
@@ -21,6 +21,7 @@ public class UIElementsDisplay extends UIComponent {
     private final Table buttonTable = new Table();
     private TextButton remainingMobsButton;
     private TextButton timerButton;
+    long time = 0;
 
     @Override
     public void create() {
@@ -98,7 +99,14 @@ public class UIElementsDisplay extends UIComponent {
                     createTimerButton();
                 }
                 timerButton.setText("Next wave in: " + finalTime);
+                time = ServiceLocator.getTimeSource().getTime();
             } else {
+                if (ServiceLocator.getTimeSource().getTime() <  time + 2000) {
+                    ServiceLocator.getMapService().shakeCameraMap();
+                    ServiceLocator.getMapService().shakeCameraGrid();
+                }
+//                ServiceLocator.getMapService().shakeCameraMap();
+//                ServiceLocator.getMapService().shakeCameraGrid();
                 remainingMobsButton.setDisabled(true);
                 timerButton.addAction(new SequenceAction(Actions.fadeOut(1f), Actions.removeActor()));
                 stage.act();
