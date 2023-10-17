3330c638ae9a5c985f88834c502e1fe1904affe5
gregchan550 <86044792+gregchan550@users.noreply.github.com>
2023-10-17 11:49:43 +1000

    changed melee attack range to 0

--- source/core/src/main/com/csse3200/game/components/tasks/MobTask/MobTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/MobTask/MobTask.java
@@ -32,7 +32,7 @@ public class MobTask extends DefaultTask implements PriorityTask {
     private static final int MELEE_DAMAGE = 10;
     private static final long MELEE_ATTACK_SPEED = 2000;
     private static final long RANGE_ATTACK_SPEED = 5000;
-    private static final float MELEE_ATTACK_RANGE = 0.2f;
+    private static final float MELEE_ATTACK_RANGE = 0f;
 
     // Private variables
     private final MobType mobType;
